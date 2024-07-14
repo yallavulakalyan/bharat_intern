febe3e4b7f7a7eea5e50234ae6541bee6243ed82
yallavulakalyan <yallavulakalyan10@gmail.com>
2024-07-14 19:06:17 +0530

    Add files via upload

---
+++ Registration form/index.js
@@ -0,0 +1,48 @@
+var express=require("express")
+var bodyParser=require("body-parser")
+var mongoose=require("mongoose")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+    extended:true
+}))
+mongoose.connect('mongodb://localhost:27017/')
+var db=mongoose.connection
+db.on('error',()=> console.log("Error in coonecting to database"))
+db.once('open',()=> console.log("connected to database"))
+
+app.post("/sign_up",(req,res)=> {
+    var name=req.body.name
+    var age=req.body.age
+    var email=req.body.email
+    var phno=req.body.phno
+    var gender=req.body.gender
+    var password=req.body.password
+    var data={
+        "name":name,
+        "age":age,
+        "email":email,
+        "phno":phno,
+        "gender":gender,
+        "password":password
+    }
+    db.collection('users').insertOne(data,(err,collection) => {
+        if(err){
+            throw err;
+        }
+        console.log("Record inserted successfully")
+    })
+    return res.redirect('signup_successful.html')
+})
+app.get("/",(req,res) => {
+    res.set({
+        "Allow-acces-Allow-Origin":'*'
+    })
+    return res.redirect('index.html')
+}).listen(3002);
+
+console.log("listening on port 3002")
+ 
